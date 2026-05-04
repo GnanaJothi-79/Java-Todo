421cf0deb46e3183e83ad7a8343c821edffbdd5d
GnanaJothi-79 <gnana07092004@gmail.com>
2026-05-04 16:00:03 +0530

    target

---
+++ src/test/java/com/todo/TestTodo.java
@@ -0,0 +1,12 @@
+package com.todo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTodo {
+
+    @Test
+    void testSample() {
+        assertEquals(1, 1);
+    }
+}
